dfe2ea71ff81f11eb0812466aa4508d9a9a32eb4
James Chen <r11922014@ntu.edu.tw>
2023-03-24 15:12:14 +0800

    Remove unused test blocks

--- align.py
+++ align.py
@@ -62,10 +62,10 @@ def get_shift(ref_image, shift_image, level):
 
     return best_shift
 
-def align(images, max_offset=64, filename=None):
+def align(images, max_shift=64):
     # Let's pick the first image as reference image
     result = [images[0]]
-    pyramid_level = int(np.log2(max_offset))
+    pyramid_level = int(np.log2(max_shift))
     for image in images[1:]:
         rshift, cshift = get_shift(images[0], image, pyramid_level)
         # print(rshift, cshift)
@@ -78,36 +78,7 @@ def align(images, max_offset=64, filename=None):
     return result
 
 def test():
-    image = cv2.imread("exposures/img01.jpg")
-    gray_image = grayscale(image)
-    cv2.imwrite("exposures/gray_img01.jpg", gray_image)
-    downsampled_gray_image = downsample(gray_image, 2)
-    cv2.imwrite("exposures/gray_down_img01.jpg", downsampled_gray_image)
-    shift_image = image_shift(gray_image, 100, 500)
-    cv2.imwrite("exposures/gray_shift_img01.jpg", shift_image)
-    a, b = MTB_and_exclusive_bm(image)
-    """
-    images = []
-    for i in range(1, 14):
-        images.append(cv2.imread(f"exposures/img{i:02}.jpg"))
-    res = align(images)
-    for i, img in enumerate(res):
-        img *= 255
-        img = img.astype(int)
-        cv2.imwrite(f"exposures/align_img{i+1:02}.jpg", img)
-    """
-    base = "pic2"
-    images = []
-    images.append(cv2.imread(os.path.join(base, "1.JPG")))
-    print(f"fuck {images[0].shape}")
-    filename = ["1.JPG"]
-    for f in sorted(os.listdir(base)):
-        if f[-3:] != "txt" and f != "1.jpg" and f != "1.JPG":
-            images.append(cv2.imread(os.path.join(base, f)))
-            filename.append(f)
-    res = align(images, filename=filename)
-    for i, img in enumerate(res):
-        cv2.imwrite(f"output/align_{filename[i]}", img)
+    parse_args
 
 if __name__ == "__main__":
     test()
