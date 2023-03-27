a96a3ff41a50e774ef143b470a8b1a72082f6fe2
lilihsu <a940139kevin@gmail.com>
2023-03-27 19:12:35 +0800

    tone_mapping globalOperator script

--- tone_mapping.py
+++ tone_mapping.py
@@ -3,6 +3,7 @@ import cv2
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 import hdr
 import align
@@ -14,11 +15,24 @@ def luminance(img):
     output_img[:, :] = img[:, :, BLUE] * 0.0721 + img[:, :, GREEN] * 0.7154 + img[:, :, RED] * 0.2125
     return output_img
 
+def globalOperator(hdrImg, alpha = 0.8):
+    pixels = luminance(hdrImg)
+    epsilon = 0.0000001
+    height, width = pixels.shape
+    print(height, width)
+    pixels[pixels < epsilon] = epsilon
+    pixels = np.log(pixels)
+
+    lw_avg = np.sum(pixels) / (height * width)
+    print(lw_avg)
+
 def test():
     image = cv2.imread("output/result.hdr")
-    lum = luminance(image)
-    print(lum.shape)
-    print(np.where(lum != 0.0))
+    # lum = luminance(image)
+    # print(lum.shape)
+    # print(np.where(lum != 0.0))
+
+    globalOperator(image)
 
 if __name__ == "__main__":
     test()
